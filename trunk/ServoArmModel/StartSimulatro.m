2d9a35bb41d96f18d4c5a8939c68ba632a01fe09
kristijan.petkov@gmail.com <kristijan.petkov@gmail.com@68284825-9aae-d6fb-1af0-d734d2c72a41>
2012-10-21 19:37:18 +0000

    Add new arm model

--- trunk/ServoArmModel/StartSimulatro.m
+++ trunk/ServoArmModel/StartSimulatro.m
@@ -16,5 +16,48 @@ joint3Val = 0;
 
 %% Show the GUI and run the sim
 
-hanlde = UIJointJog;
+%hanlde = UIJointJog;
+
+% world frame 
+% x+ is to the right 
+% y+ is pointing up
+% z+ is towards me
+worldFrame = [ 1, 0, 0, 0;
+               0, 1, 0, 0;
+               0, 0, 1, 0;
+               0, 0, 0, 1;
+    ];
+
+% current robot orientation
+% x+ is to the right
+% y+ is towrd me
+% z+ is down
+FromRobotToWorld =   [1, 0,  0,   -0.5;
+                      0, 0, -1,    0;
+                      0, 1,  0,   -0.51;
+                      0, 0,  0,   1;
+    ];
+FromWorldToRobot = inv(FromRobotToWorld)
+
+CenterRobotInWorldFrame=[0.5, 0.51, 0, 1]';
+CenterWorldFrame = [0,0,0,1]';
+
+% transformation from  frame to robot frame
+FromRobotToWorld * CenterRobotInWorldFrame ;
+
+% transformation from world frame to robot frame
+FromWorldToRobot * CenterWorldFrame ;
+
+
+%%
+world = vrworld('ArmModel3D.wrl', 'new');
+open(world);
+fig = vrfigure(world);
+baseCube = vrnode(world, 'Base');
+disp(baseCube.translation);% = [-.5, -.51, 0];
+disp(baseCube.rotation);
+for i=0:0.1:2*pi
+    pause(0.1)
+    baseCube.rotation = [0,1,0,i];
+end
 %sim('JointJogSim');
