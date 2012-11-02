c38435749aac76504ad70e08079b61b66ad99090
kristijan.petkov@gmail.com <kristijan.petkov@gmail.com@68284825-9aae-d6fb-1af0-d734d2c72a41>
2012-11-02 20:59:53 +0000

    sim functional

--- trunk/ServoArmModel/JointValuesToVRTranformations.m
+++ trunk/ServoArmModel/JointValuesToVRTranformations.m
@@ -172,7 +172,7 @@ theta2 = joint2Val;
 
 %rotate by joint1 rotation
 Joint2Frame = Joint1Frame * Joint2Frame;
-Joint2Frame = Joint2Frame * rothz(theta2);
+Joint2Frame = Joint2Frame * rothz(90 + theta2);
 
 Joint2Offset = Joint2Frame * VR_center';
 Joint2Offset = Joint2Offset(1:3)'; %3d vector no homogenious
@@ -180,7 +180,7 @@ Joint2Offset = Joint2Offset(1:3)'; %3d vector no homogenious
 % get rotation in VR world
 %VR_Joint2Frame = VR_Joint2Frame * VR_Joint1Frame;
 VR_Joint2Frame =  rothy(theta0) * rothz(theta1) * ...
-                  rothz(theta2) * VR_Joint2Frame;
+                  rothz(90 + theta2) * VR_Joint2Frame;
 
 VR_Joint2Position = GetTranslationFromFrame(VR_Joint2Frame);
 VR_Joint2Translation = (-1)* VR_Joint2Position(1:3) + Joint2Offset;
@@ -221,7 +221,7 @@ Joint3Offset = Joint3Offset(1:3)'; %3d vector no homogenious
 
 %VR_Joint3Frame = VR_Joint2Frame * VR_Joint3Frame;
 VR_Joint3Frame = rothy(theta0) * rothz(theta1) * ...
-                  rothz(theta2) *rothz(theta3) * VR_Joint3Frame;
+                  rothz(90 + theta2) *rothz(theta3) * VR_Joint3Frame;
 
 VR_Joint3Position = GetTranslationFromFrame(VR_Joint3Frame);
 VR_Joint3Translation = (-1)* VR_Joint3Position(1:3) + Joint3Offset;
