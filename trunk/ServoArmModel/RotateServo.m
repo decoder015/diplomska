6e902c5d0081f30a151b649744120135341f4bca
kristijan.petkov@gmail.com <kristijan.petkov@gmail.com@68284825-9aae-d6fb-1af0-d734d2c72a41>
2012-11-04 17:28:26 +0000

    Improve model

--- trunk/ServoArmModel/RotateServo.m
+++ trunk/ServoArmModel/RotateServo.m
@@ -1,25 +1,18 @@
 %----------------------------------------------------
 % parameters: 
 % comPort:      serial port name e.g. 'COM13'
+% port state:   0 closed, 1 opend
 % servo number: 0-4, 
 % angle:        in degreees
 %----------------------------------------------------
 function RotateServo(comPort, sevoNumber, angle)
-%% Open serial port
-serialPort=serial(comPort', 'BaudRate', 9600, 'DataBits',8);
-serialPort.terminator = 'CR';
-fopen(serialPort);
 %% Wrte to serial port
 % write to port
 % mini SSC 2 syntx [sync] [servo] [positon]
 % eg. 255 0 
 position = angles2servoPos(angle);
-disp(position);
-fwrite(serialPort, 255);          %[sync]
-fwrite(serialPort, sevoNumber);   %[servo]
-fwrite(serialPort, position);     %[positon]
-
-pause(0.3);
-%% close serial port
-fclose(serialPort);
-end
+%disp(position);
+fwrite(comPort, 255);          %[sync]
+fwrite(comPort, sevoNumber);   %[servo]
+fwrite(comPort, position);     %[positon]
+%=======================END=========================
