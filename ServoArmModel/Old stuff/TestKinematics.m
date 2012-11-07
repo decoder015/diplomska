0526aa6252f8977a83a366c0771b34e6a23267e1
kristijan.petkov <kristijan.petkov@gmail.com>
2012-11-07 20:49:44 +0000

    test ikin

--- ServoArmModel/Old stuff/TestKinematics.m
+++ ServoArmModel/Old stuff/TestKinematics.m
@@ -12,10 +12,24 @@ worldFrame = [ 1, 0, 0, 0;
                0, 0, 0, 1;
     ];
 %%
-Joint0Frame =  transl(0, 0.06, 0)
-Joint0Frame = Joint0Frame * rothx(-90);
-disp(Joint0Frame);
+%Joint0Frame =  transl(0,a 0.06, 0)
+%Joint0Frame =t Joint0Frame * rothx(-90);
+%disp(Joint0Frame);
 %frInv = inv(Joint0Frame)
 
-rotation = Joint0Frame * rothz(0)
-rotVect = getZ(Joint0Frame);
+%rotation = Joint0Frame * rothz(0)
+%rotVect = getZ(Joint0Frame);
+%%
+L(1) = Link([ 0	0 pi/2 0], 'standard');
+L(2) = Link([ 0	0 pi/2 0], 'standard');
+%twolink = SerialLink(L, 'name', 'two link');
+%twolink.plot([0 0 0 0 0]);
+%%
+L(1) = Link([ 0     0   2  0], 'standard');
+L(2) = Link([ pi/2     0   3  pi/2], 'standard');
+twolink = SerialLink(L, 'name', 'two link')
+T=rpy2tr(0,0,0); 
+T(1:2, 4)=[1 1]
+Qsol=twolink.ikine(T, zeros(1,2), [1 1 0 0 0 0]);
+twolink.plot([0,pi/2])
+
