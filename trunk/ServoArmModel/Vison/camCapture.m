94f7856c8a26477253c5ca77e57ba6dcff41ad13
kristijan.petkov@gmail.com <kristijan.petkov@gmail.com@68284825-9aae-d6fb-1af0-d734d2c72a41>
2012-11-14 23:20:19 +0000

    gui

--- trunk/ServoArmModel/Vison/camCapture.m
+++ trunk/ServoArmModel/Vison/camCapture.m
@@ -3,11 +3,11 @@ close all;
 clear all;
 %%
 %open video
-cam = videoinput('winvideo', 1, 'YUY2_640x480');
+cam =  videoinput('winvideo', 1, 'RGB24_640x480');
 
  src_obj = getselectedsource(cam);
  get(src_obj)
-
+subplot(2,2,1);
 preview(cam)
 disp ('end');
 
